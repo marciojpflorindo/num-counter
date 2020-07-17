726ab9c4bd0fdea2052f54cf7389a5c35de1f7c1
Márcio Florindo <61458507+marciojpflorindo@users.noreply.github.com>
2020-07-17 08:54:33 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+let count = 0;
+
+function minus() {
+  count -= 1;
+  document.querySelector('.result').innerHTML = count;
+
+}
+function plus() {
+  count += 1;
+  document.querySelector('.result').innerHTML = count;
+
+}
